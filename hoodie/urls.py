bce46a0b591e234c775a436b76e5c470abc1e5d0
bellahOchola <bellahkenya@gmail.com>
2020-01-16 16:14:52 +0300

    create user authentication system

--- hoodie/urls.py
+++ hoodie/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path()
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
--- hoodie/views.py
+++ hoodie/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.post)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/signup.html', {'form':form})
